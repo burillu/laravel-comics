c304f72fca479cc29f5cc2f6f64e6389d4fa5892
burillu <burillu90@hotmail.it>
2023-12-18 16:59:26 +0100

    printing comics in main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,23 @@
 
 @section('content')
 <main>
-    Main
+    <div class="container">
+        <div class="row">
+            @foreach ($comics as $comic)
+                <div class="col-12 col-md-4">
+                <div class="card">
+                    <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$comic['title']}}</h5>
+                        <p class="card-text">{{$comic['description']}}</p>
+                        
+                      </div>
+                </div>
+            </div>
+            @endforeach
+            
+        </div>
+    </div>
 </main>
 
 @endsection
