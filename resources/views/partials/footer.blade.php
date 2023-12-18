022dbfa160d2af9a04e7a5f782d80fade788d950
burillu <burillu90@hotmail.it>
2023-12-18 16:31:10 +0100

    created base layout

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,13 @@
+<footer>
+    
+    </footer>
+    
+    <ul>
+    <li class="text-white text-uppercase ">titolo </li>
+    <li >
+        <a > link </a>
+    </li>
+    <li >
+        <a > link </a>
+    </li>
+</ul>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,17 @@
+<header>
+<nav class="">
+
+<ul class="nav text-secondary fw-bold  text-uppercase">
+    <li class="nav-item">
+        <a class="nav-link ">menu</a>
+
+    </li>
+    <li class="nav-item">
+        <a class="nav-link ">menu</a>
+
+    </li>
+    
+
+</ul>
+</nav>
+</header>
